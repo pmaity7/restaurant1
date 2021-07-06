2483a967952ed3bb43f25d18283c1a3819344f94
Pallavi Maity <dmaity897@gmail.com>
2021-07-06 19:05:31 +0530

    webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { mainModule } = require('process');
 
 module.exports = {
-    mode: development,
+    mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'main.js',
